b47ea26cc7450827041e65f6ecca387cb77883a3
davidluzhiyun <david.luzhiyun@gmail.com>
2024-12-12 08:32:31 -0500

    Boundry works somehow, I guess quartus doesn't like too many comments?

--- PS2_skeleton_restored-2/assembly_code/space_ship_test2.s
+++ PS2_skeleton_restored-2/assembly_code/space_ship_test2.s
@@ -6,10 +6,12 @@ addi $7, $0, 5000000    # r7 is upper limit for move counter
 addi $4, $0, 320           # r4 = 320 (r4 is spaceshipx)
 addi $5, $0, 0               # r5 = 0 (r5 is movecouter)
 nop
-blt $5, $7, 3                 # don't move and skip to normal routine if r5<r7
+blt $5, $7,  5                # don't move and skip to normal routine if r5<r7
 addi $5, $0, 0
-add $4, $4, $2             # Attempt move right
+blt $4, $0, 1
 sub $4, $4, $1             # Attempt move left
+blt $6, $4, 1
+add $4, $4, $2             # Attempt move right
 
 # normal routine
 addi $5, $5, 1    #increment counter
